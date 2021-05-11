f9304d25739b1ec19c85603a0d51118724c1d420
Solon P <freeberd83@gmail.com>
2021-05-11 20:31:00 +0300

    Add files via upload

---
+++ astronaut.py
@@ -0,0 +1,11 @@
+def main():
+    height = float(input("Enter your height in meters: "))
+    if height >= 1.6 and height <= 1.9:
+        print("Correct height to be an astronaut")
+    elif height < 1.6:
+        print("Below minimum astronaut height")
+    else:
+        print("Above maximum astronaut height")        
+
+if __name__ == "__main__":
+    main()
